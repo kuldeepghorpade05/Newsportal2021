35b257b3e92c0d97a7ce3803ba15eee005f1422c
kuldeep ghorpade <kuldeepghorpade05@gmail.com>
2023-08-25 13:10:32 +0530

    New project  added Newsportal2021

---
+++ admin/includes/footer.php
@@ -0,0 +1,4 @@
+
+                <footer class="footer text-right">
+                   2022 © Developed by RCS Technology.
+                </footer>
---
+++ includes/header.php
@@ -0,0 +1,34 @@
+ <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="index.php"><img src="images/logo.png" height="50"></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="about-us.php">About</a>
+            </li>
+              <li class="nav-item">
+              <a class="nav-link" href="category.php?catid=9">Covid-19</a>
+            </li>
+			 <li class="nav-item">
+              <a class="nav-link" href="category.php?catid=6">Politics</a>
+            </li>
+			
+			 <li class="nav-item">
+              <a class="nav-link" href="category.php?catid=7">Business</a>
+            </li>
+			 <li class="nav-item">
+              <a class="nav-link" href="category.php?catid=7">International</a>
+            </li>
+			
+             <li class="nav-item">
+              <a class="nav-link" href="contact-us.php">Contact us</a>
+            </li>
+  
+  
+          </ul>
+        </div>
+      </div>
+    </nav>
